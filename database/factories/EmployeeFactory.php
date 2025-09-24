eb923ea0c658d306cac3d4daf62dc2d63b420644
fathanmaul <fthnmrh012@gmail.com>
2025-09-24 10:45:28 +0700

    feat(model) : adding employee model + seeder & factory

---
+++ database/factories/EmployeeFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Employee>
+ */
+class EmployeeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $name = $this->faker->name();
+        return [
+            'name' => $name,
+            'email' => $this->faker->unique()->safeEmail(),
+            'address' => $this->faker->address(),
+            'phone' => $this->faker->unique()->phoneNumber(),
+            'user_picture' => 'https://ui-avatars.com/api/?name='.$name,
+            'password' => bcrypt('password'),
+        ];
+    }
+}
